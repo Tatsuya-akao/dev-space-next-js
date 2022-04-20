1de170a030da262ec9bc781ddfcb582dee1c28e7
tatsuya <a.tatsu0117@gmail.com>
2022-04-21 00:10:02 +0900

    finished all pages

--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -1,53 +1,88 @@
 import fs from "fs";
 import path from "path";
-import Link from "next/link";
-import matter from "gray-matter";
-import Layout from "../../../components/Layout";
-import Post from "../../../components/Post";
-import { sortByDate } from "../../../utils";
+import Layout from "@/components/Layout";
+import Post from "@/components/Post";
+import Pagination from "@/components/Pagination";
+import CategoryList from "@/components/CategoryList";
+import { POSTS_PER_PAGE } from "@/config/index";
+import { getPosts } from "@/lib/posts";
 
-export default function BlogPage({ posts }) {
+export default function BlogPage({ posts, numPages, currentPage, categories }) {
   return (
     <Layout>
-      <h1 className="text-5xl border-b-4 p-5 font-bold">Blog</h1>
+      <div className="flex justify-between flex-col md:flex-row">
+        <div className="w-3/4 mr-10">
+          <h1 className="text-5xl border-b-4 p-5 font-bold">Blog</h1>
 
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {posts.map((post) => (
-          <Post key={post.slug} post={post} />
-        ))}
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
+          </div>
+
+          <Pagination currentPage={currentPage} numPages={numPages} />
+        </div>
+
+        <div className="w-1/4">
+          <CategoryList categories={categories} />
+        </div>
       </div>
     </Layout>
   );
 }
 
 export async function getStaticPaths() {
+  // get all '.md' files in '/posts' directory
   const files = fs.readdirSync(path.join("posts"));
 
-  const numPages = 3;
+  // the number of all archive pages
+  const numPages = Math.ceil(files.length / POSTS_PER_PAGE);
+
+  let paths = [];
+
+  for (let i = 1; i <= numPages; i++) {
+    paths.push({
+      params: { page_index: i.toString() },
+    });
+  }
+
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
-export async function getStaticProps() {
+export async function getStaticProps({ params }) {
+  // current page index
+  const page = parseInt((params && params.page_index) || 1);
+
+  // get all '.md' files in '/posts' directory
   const files = fs.readdirSync(path.join("posts"));
 
-  const posts = files.map((filename) => {
-    const slug = filename.replace(".md", "");
+  const posts = getPosts();
 
-    const markdownWithMeta = fs.readFileSync(
-      path.join("posts", filename),
-      "utf-8"
-    );
+  // get categories for sidebar
+  const categories = posts.map((post) => post.frontmatter.category);
 
-    const { data: frontmatter } = matter(markdownWithMeta);
+  const uniqueCategories = [...new Set(categories)];
 
-    return {
-      slug,
-      frontmatter,
-    };
-  });
+  // the number of all archive pages
+  const numPages = Math.ceil(files.length / POSTS_PER_PAGE);
+
+  // slice method starts from 0, so we need to subtract 1
+  // ex) slice(0, 3), slice(3, 6)
+  const pageIndex = page - 1;
+  const orderedPosts = posts.slice(
+    pageIndex * POSTS_PER_PAGE,
+    (pageIndex + 1) * POSTS_PER_PAGE
+  );
 
   return {
     props: {
-      posts: posts.sort(sortByDate),
+      posts: orderedPosts,
+      numPages, // all archive pages
+      currentPage: page, // current page index
+      categories: uniqueCategories,
     },
   };
 }
--- tasks.md
+++ tasks.md
@@ -1,13 +1,20 @@
 # Tasks(Must)
 
-- Add dark/light mode functionality
 - Add pagination
 - Add Contact page
 
 # Tasks(Optional)
 
+## Add Pages / Functionality
+
+- Add dark/light mode functionality
+- Add Tag functionality
 - Likes counter on every post
+- Add 'Revised Date' field on front matter
+- Add page transition animation
+
+## Setup
+
 - Place something like 'Buy me a coffee'
 - Google Analytics
 - Google Adsense
-- Add 'Revised Date' field on front matter
